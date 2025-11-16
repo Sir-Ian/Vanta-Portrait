7fc77750d86c0ead943654f48458a80a388fdf81
Sir-Mac <148124526+Sir-Ian@users.noreply.github.com>
2025-11-15 23:11:05 -0600

    Initial Commit

---
+++ Vanta Portrait/Vanta_PortraitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vanta_PortraitApp.swift
+//  Vanta Portrait
+//
+//  Created by Ian Deuberry on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Vanta_PortraitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Vanta PortraitTests/Vanta_PortraitTests.swift
@@ -0,0 +1,16 @@
+//
+//  Vanta_PortraitTests.swift
+//  Vanta PortraitTests
+//
+//  Created by Ian Deuberry on 11/15/25.
+//
+
+import Testing
+
+struct Vanta_PortraitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
